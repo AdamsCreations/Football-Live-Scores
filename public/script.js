4e0288704e22f4042a272849aa51e514e2790391
AdamsCreations <adamsalahahmed@gmail.com>
2026-05-21 14:22:38 +0300

    add today's matches and standings refresh

--- public/script.js
+++ public/script.js
@@ -9,17 +9,21 @@ async function getTodaysMatches () {
     };
 };
 
-getTodaysMatches();
-
-document.getElementById("todays-matches-btn").addEventListener("click",async () => {
+async function getStandings () {
     try{
-        const request = await fetch("/todaysMatches",{headers: {"x-localDate": new Date().toLocaleDateString('en-CA')}});
+        const request = await fetch("/standings");
         const response = await request.json();
-        console.log(response); 
+        console.log(response);
     }catch(error){
         console.log(error.message);
     };
-})
+};
+
+getTodaysMatches();
+setInterval(getTodaysMatches, 60000);
+setInterval(getStandings, 60000);
+
+document.getElementById("todays-matches-btn").addEventListener("click", getTodaysMatches);
 
 document.getElementById("fixtures-btn").addEventListener("click",async  () => {
     try{
@@ -30,7 +34,7 @@ document.getElementById("fixtures-btn").addEventListener("click",async  () => {
         console.log(error.message);
     };
 
-})
+});
 
 document.getElementById("results-btn").addEventListener("click",async () =>{
     try{
@@ -40,17 +44,9 @@ document.getElementById("results-btn").addEventListener("click",async () =>{
     }catch(error){
         console.log(error.message);
     };
-})
-
-document.getElementById("standings-btn").addEventListener("click",async () =>{
-    try{
-        const request = await fetch("/standings");
-        const response = await request.json();
-        console.log(response);
-    }catch(error){
-        console.log(error.message);
-    };
 });
+
+document.getElementById("standings-btn").addEventListener("click", getStandings);
 
 document.getElementById("top-goal-scorers").addEventListener("click",async () =>{
     try{
@@ -60,4 +56,4 @@ document.getElementById("top-goal-scorers").addEventListener("click",async () =>
     }catch(error){
         console.log(error.message);
     };
-})
+});
