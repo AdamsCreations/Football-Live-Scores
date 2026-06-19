48466dffa67a09c67909743c7c8ec587cc701d06
Adam Salah <adamsalahahmed930@gmail.com>
2026-06-19 14:52:40 +0300

    add today's matches and standings parsing from the backend to css elements

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import { getCache } from './cache.js';
-import { setCache } from './cache.js';
+import { getCache } from "./cache.js";
+import { setCache } from "./cache.js";
 
 const app = express();
 const port = 3000;
-const today = new Date().toISOString().split('T')[0];
+const today = new Date().toISOString().split("T")[0];
+const testDate = "2026-04-12";
 const api = axios.create({
   baseURL: "http://api.football-data.org/v4",
-  headers:{
-    "X-Auth-Token": process.env.FD_API_KEY
-  }
+  headers: {
+    "X-Auth-Token": process.env.FD_API_KEY,
+  },
 });
 
-function matchesFilter(obj){
+function matchesFilter(obj) {
   return {
     utcDate: obj.utcDate,
     status: obj.status,
+    matchday: obj.matchday,
     homeTeamName: obj.homeTeam.shortName,
     homeTeamCrest: obj.homeTeam.crest,
     awayTeamName: obj.awayTeam.shortName,
     awayTeamCrest: obj.awayTeam.crest,
-    homeScore: obj.score.fullTime.home,
-    awayScore: obj.score.fullTime.away,
-  }
-};
+    homeScore: obj.score.fullTime.home ?? "",
+    awayScore: obj.score.fullTime.away ?? "",
+  };
+}
 
-function standingsFilter(obj){
-  return{
+function standingsFilter(obj) {
+  return {
     position: obj.position,
     team: obj.team.name,
     crest: obj.team.crest,
@@ -40,113 +42,119 @@ function standingsFilter(obj){
     points: obj.points,
     goalsFor: obj.goalsFor,
     goalsAgainst: obj.goalsAgainst,
-    goalDifference: obj.goalDifference
-  }
-};
+    goalDifference: obj.goalDifference,
+  };
+}
 
-function topGoalScorersFilter(obj){
-  return{
+function topGoalScorersFilter(obj) {
+  return {
     playerName: obj.player.name,
     teamName: obj.team.shortName,
     teamCrest: obj.team.crest,
     goals: obj.goals,
-    penalties: obj.penalties??0
-  }
-};
+    penalties: obj.penalties ?? 0,
+  };
+}
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",async (req, res) => {
+app.get("/", async (req, res) => {
   console.log("GET /");
-  try{
+  try {
     console.log("server get cache");
     const cache = getCache("standings");
-    if(cache){
+    if (cache) {
       console.log("cache is available");
-      res.render("index.ejs", {standings: JSON.stringify(cache)});
-    }else{
+      res.render("index.ejs", { standings: JSON.stringify(cache.data) });
+    } else {
       const standingsRequest = await api.get("/competitions/PL/standings");
       const standingsResponse = standingsRequest.data.standings[0].table;
       const data = standingsResponse.map(standingsFilter);
       console.log("no cache");
       console.log("server set cache");
       setCache("standings", data, Date.now());
-      res.render("index.ejs", {standings: JSON.stringify(data)});
-    };
-  }catch(error){
+      res.render("index.ejs", { standings: JSON.stringify(data) });
+    }
+  } catch (error) {
     console.log(error.message);
-  };
+  }
 });
 
-app.get("/todaysMatches",async (req, res) =>{
-  try{
+app.get("/todaysMatches", async (req, res) => {
+  try {
     const cache = getCache("todays-matches");
-    if(cache){
+    if (cache) {
       console.log("cache is available");
-      res.json(cache);
-    }else{
+      res.json(cache.data);
+    } else {
       const localDate = req.headers["x-localDate"] || today;
-      const matchesRequest = await api.get("/competitions/PL/matches",{params:{dateFrom: localDate, dateTo: localDate}});
+      const matchesRequest = await api.get("/competitions/PL/matches", {
+        params: { dateFrom: testDate, dateTo: testDate },
+      });
       const matchesResponse = matchesRequest.data.matches;
       const data = matchesResponse.map(matchesFilter);
       console.log("no cache");
       console.log("server set cache");
       setCache("todays-matches", data, Date.now());
       res.json(data);
-    };
-  }catch(error){
+    }
+  } catch (error) {
     console.log(error.message);
-  };
-})
+  }
+});
 
-app.get("/fixtures",async (req, res) => {
-  try{
+app.get("/fixtures", async (req, res) => {
+  try {
     const cache = getCache("fixtures");
-    if(cache){
+    if (cache) {
       console.log("cache is available");
-      res.json(cache);
-    }else{
-      const matchesRequest = await api.get("/competitions/PL/matches",{params:{status: "SCHEDULED"}});
+      res.json(cache.data);
+    } else {
+      const matchesRequest = await api.get("/competitions/PL/matches", {
+        params: { status: "SCHEDULED" },
+      });
       const matchesResponse = matchesRequest.data.matches;
       const data = matchesResponse.map(matchesFilter);
       console.log("no cache");
       console.log("server set cache");
       setCache("fixtures", data, Date.now());
       res.json(data);
-    };
-  }catch(error){
+    }
+  } catch (error) {
     console.log(error.message);
-  };
+  }
 });
 
-app.get("/results",async (req, res) => {
-  try{
+app.get("/results", async (req, res) => {
+  try {
     const cache = getCache("results");
-    if(cache){
+    if (cache) {
       console.log("cache is available");
-      res.json(cache);
-    }else{
-      const matchesRequest = await api.get("/competitions/PL/matches",{params:{status: "FINISHED"}});
+      res.json(cache.data);
+    } else {
+      const matchesRequest = await api.get("/competitions/PL/matches", {
+        params: { status: "FINISHED" },
+      });
       const matchesResponse = matchesRequest.data.matches;
       const data = matchesResponse.map(matchesFilter);
       console.log("no cache");
       console.log("server set cache");
       setCache("results", data, Date.now());
       res.json(data);
-    };
-  }catch(error){
+    }
+  } catch (error) {
     console.log(error.message);
-  };
+  }
 });
 
-app.get("/standings",async (req, res) => {
-  try{
+app.get("/standings", async (req, res) => {
+  try {
     const cache = getCache("standings");
-    if(cache){
+    if (cache) {
       console.log("cache is available");
-      res.json(cache);
-    }else{
+      res.json(cache.data);
+    } else {
       const standingsRequest = await api.get("/competitions/PL/standings");
       const standingsResponse = standingsRequest.data.standings[0].table;
       const data = standingsResponse.map(standingsFilter);
@@ -154,19 +162,19 @@ app.get("/standings",async (req, res) => {
       console.log("server set cache");
       setCache("standings", data, Date.now());
       res.json(data);
-    };
-  }catch(error){
+    }
+  } catch (error) {
     console.log(error.message);
-  };
-})
+  }
+});
 
-app.get("/topGoalScorers",async (req, res) =>{
-  try{
+app.get("/topGoalScorers", async (req, res) => {
+  try {
     const cache = getCache("top-goal-scorers");
-    if(cache){
+    if (cache) {
       console.log("cache is available");
-      res.json(cache);
-    }else{
+      res.json(cache.data);
+    } else {
       const topGoalScorersRequest = await api.get("/competitions/PL/scorers");
       const topGoalScorersResponse = topGoalScorersRequest.data.scorers;
       const data = topGoalScorersResponse.map(topGoalScorersFilter);
@@ -174,10 +182,10 @@ app.get("/topGoalScorers",async (req, res) =>{
       console.log("server set cache");
       setCache("top-goal-scorers", data, Date.now());
       res.json(data);
-    };
-  }catch(error){
+    }
+  } catch (error) {
     console.log(error.message);
-  };
+  }
 });
 
 app.listen(port, () => {
--- public/script.js
+++ public/script.js
@@ -1,59 +1,177 @@
+function parseTodaysMatchesJSON(jsonResponse) {
+  document
+    .querySelectorAll(".todays-matches-item")
+    .forEach((item) => item.remove());
+  jsonResponse.forEach((match) => {
+    const matchCard = document.createElement("li");
+    matchCard.classList.add("todays-matches-item");
+    matchCard.classList.add("match-card");
+    matchCard.classList.add(match.status);
+    document.getElementById("todays-matches").appendChild(matchCard);
 
-async function getTodaysMatches () {
-    try{
-        const request = await fetch("/todaysMatches",{headers: {"x-localDate": new Date().toLocaleDateString('en-CA')}});
-        const response = await request.json();
-        console.log(response);
-    }catch(error){
-        console.log(error)
-    };
-};
+    const time = matchCard.appendChild(document.createElement("p"));
+    time.classList.add("time");
 
-async function getStandings () {
-    try{
-        const request = await fetch("/standings");
-        const response = await request.json();
-        console.log(response);
-    }catch(error){
-        console.log(error.message);
-    };
-};
+    const homeTeamCrest = matchCard.appendChild(document.createElement("img"));
+    homeTeamCrest.classList.add("homeTeamCrest");
+
+    const homeTeamName = matchCard.appendChild(document.createElement("p"));
+    homeTeamName.classList.add("homeTeamName");
+
+    const homeTeamScore = matchCard.appendChild(document.createElement("p"));
+    homeTeamScore.classList.add("homeTeamScore");
+
+    const awayTeamCrest = matchCard.appendChild(document.createElement("img"));
+    awayTeamCrest.classList.add("awayTeamCrest");
+
+    const awayTeamName = matchCard.appendChild(document.createElement("p"));
+    awayTeamName.classList.add("awayTeamName");
+
+    const awayTeamScore = matchCard.appendChild(document.createElement("p"));
+    awayTeamScore.classList.add("awayTeamScore");
+
+    time.innerText = new Date(match.utcDate).toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "2-digit",
+      hour12: true,
+    });
+    homeTeamCrest.setAttribute("src", match.homeTeamCrest);
+    homeTeamName.innerText = match.homeTeamName;
+    homeTeamScore.innerText = match.homeScore;
+    awayTeamCrest.setAttribute("src", match.awayTeamCrest);
+    awayTeamName.innerText = match.awayTeamName;
+    awayTeamScore.innerText = match.awayScore;
+  });
+}
+
+function parseStandingsJSON(jsonResponse) {
+  document.querySelectorAll(".standings-item").forEach((item) => item.remove());
+  jsonResponse.forEach((team) => {
+    const teamCard = document.createElement("li");
+    teamCard.classList.add("standings-item");
+    document.getElementById("standings").appendChild(teamCard);
+
+    const crest = teamCard.appendChild(document.createElement("img"));
+    crest.classList.add("crest");
+
+    const position = teamCard.appendChild(document.createElement("p"));
+    position.classList.add("position");
+
+    const name = teamCard.appendChild(document.createElement("p"));
+    name.classList.add("name");
+
+    const played = teamCard.appendChild(document.createElement("p"));
+    played.classList.add("played");
+
+    const won = teamCard.appendChild(document.createElement("p"));
+    won.classList.add("won");
+
+    const draw = teamCard.appendChild(document.createElement("p"));
+    draw.classList.add("draw");
+
+    const lost = teamCard.appendChild(document.createElement("p"));
+    lost.classList.add("lost");
+
+    const goalsFor = teamCard.appendChild(document.createElement("p"));
+    goalsFor.classList.add("goalsFor");
+
+    const goalsAgainst = teamCard.appendChild(document.createElement("p"));
+    goalsAgainst.classList.add("goalsAgainst");
+
+    const goalDifference = teamCard.appendChild(document.createElement("p"));
+    goalDifference.classList.add("goalDifference");
+
+    const points = teamCard.appendChild(document.createElement("p"));
+    points.classList.add("points");
+
+    crest.setAttribute("src", team.crest);
+    position.innerText = team.position;
+    name.innerText = team.team;
+    played.innerText = team.playedGames;
+    won.innerText = team.won;
+    draw.innerText = team.draw;
+    lost.innerText = team.lost;
+    goalsFor.innerText = team.goalsFor;
+    goalsAgainst.innerText = team.goalsAgainst;
+    goalDifference.innerText = team.goalDifference;
+    points.innerText = team.points;
+  });
+}
+
+async function getTodaysMatches() {
+  try {
+    const request = await fetch("/todaysMatches", {
+      headers: { "x-localDate": new Date().toLocaleDateString("en-CA") },
+    });
+    const response = await request.json();
+    parseTodaysMatchesJSON(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getFixtures() {
+  try {
+    const request = await fetch("/fixtures", {
+      headers: { "x-localDate": new Date().toLocaleDateString("en-CA") },
+    });
+    const response = await request.json();
+    console.log(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getResults() {
+  try {
+    const request = await fetch("/results", {
+      headers: { "x-localDate": new Date().toLocaleDateString("en-CA") },
+    });
+    const response = await request.json();
+    console.log(response);
+    parseResults(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getStandings() {
+  try {
+    const request = await fetch("/standings");
+    const response = await request.json();
+    console.log(Array.isArray(response));
+    parseStandingsJSON(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getTopGoalScorers() {
+  try {
+    const request = await fetch("/topGoalScorers");
+    const response = await request.json();
+    console.log(response);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 getTodaysMatches();
 setInterval(getTodaysMatches, 60000);
 setInterval(getStandings, 60000);
 
-document.getElementById("todays-matches-btn").addEventListener("click", getTodaysMatches);
+document
+  .getElementById("todays-matches-btn")
+  .addEventListener("click", getTodaysMatches);
 
-document.getElementById("fixtures-btn").addEventListener("click",async  () => {
-    try{
-        const request = await fetch("/fixtures",{headers: {"x-localDate": new Date().toLocaleDateString('en-CA')}});
-        const response = await request.json();
-        console.log(response);
-    }catch(error){
-        console.log(error.message);
-    };
+document.getElementById("fixtures-btn").addEventListener("click", getFixtures);
 
-});
+document.getElementById("results-btn").addEventListener("click", getResults);
 
-document.getElementById("results-btn").addEventListener("click",async () =>{
-    try{
-        const request = await fetch("/results",{headers: {"x-localDate": new Date().toLocaleDateString('en-CA')}});
-        const response = await request.json();
-        console.log(response);
-    }catch(error){
-        console.log(error.message);
-    };
-});
+document
+  .getElementById("standings-btn")
+  .addEventListener("click", getStandings);
 
-document.getElementById("standings-btn").addEventListener("click", getStandings);
-
-document.getElementById("top-goal-scorers").addEventListener("click",async () =>{
-    try{
-        const request = await fetch("/topGoalScorers");
-        const response = await request.json();
-        console.log(response);
-    }catch(error){
-        console.log(error.message);
-    };
-});
+document
+  .getElementById("top-goal-scorers")
+  .addEventListener("click", getTopGoalScorers);
